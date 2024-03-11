d325d8d1d186849bdd855513b50e5583e724ff8a
Dark-soul09 <122736858+Dark-soul09@users.noreply.github.com>
2024-03-11 16:34:17 +0530

    Update Q3.cpp

--- LAB-6/Q3/Q3.cpp
+++ LAB-6/Q3/Q3.cpp
@@ -1,63 +1,84 @@
 #include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define all(v) v.begin(), v.end()
+#define pii pair<int,int>
+#define Mx vector<vector<int>>
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
 using namespace std;
+bool dfs(int curr, int des, vector<vector<int> >& adj,
+            vector<int>& vis)
+{
 
-const int N = 1e5+5;
-int V, E;
-vector<int> adj[N], radj[N];
-bool vis[N], rvis[N];
-
-void dfs(int s, stack<int> &st){
-    vis[s] = 1;
-    for(auto x:adj[s])
-        if(!vis[x])
-            dfs(x, st);
-    st.push(s);
-}
-
-vector<int> topological_sort(){
-    stack<int> st;
-    vector<int> srt;
-    for(int i=0; i<V; i++)
-        if(!vis[i])
-            dfs(i, st);
-    while(!st.empty()){
-        srt.push_back(st.top());
-        st.pop();
+    if (curr == des) {
+        return true;
     }
-    return srt;
+    vis[curr] = 1;
+    for (auto x : adj[curr]) {
+        if (!vis[x]) {
+            if (dfs(x, des, adj, vis)) {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-void rdfs(int s){
-    rvis[s] = 1; cout << s << " ";
-    for(auto x:radj[s])
-        if(!rvis[x])
-            rdfs(x);
+bool isPath(int src, int des, vector<vector<int> >& adj)
+{
+    vector<int> vis(adj.size() + 1, 0);
+    return dfs(src, des, adj, vis);
 }
 
+vector<vector<int> > findSCC(int n,vector<vector<int> >& adj)
+{
+    vector<vector<int> > ans;
+    vector<int> is_scc(n + 1, 0);
+
+    // vector<vector<int> > adj(n + 1);
+
+    // for (int i = 0; i < a.size(); i++) {
+    //     adj[a[i][0]].push_back(a[i][1]);
+    // }
+
+    // Traversing all the vertices
+    for (int i = 1; i <= n; i++) {
+
+        if (!is_scc[i]) {
+            vector<int> scc;
+            scc.push_back(i);
+
+            for (int j = i + 1; j <= n; j++) {
+                if (!is_scc[j] && isPath(i, j, adj)
+                    && isPath(j, i, adj)) {
+                    is_scc[j] = 1;
+                    scc.push_back(j);
+                }
+            }
+            ans.push_back(scc);
+        }
+    }
+    return ans;
+}
 int main(){
     freopen("test_cases_Q3.txt","r",stdin);
     freopen("output_Q3.txt","w",stdout);
-    int t; cin >> t;
+    int t; cin>>t;
     while(t--){
-        cin >> V >> E;
-        for(int i=0; i<V; i++){
-            adj[i].clear();
-            radj[i].clear();
-            vis[i] = 0;
-            rvis[i] = 0;
+        int n,m; cin>>n>>m;
+        Mx adj(n+1); int u,v;
+        rep(i,0,m){
+            cin>>u>>v;
+            adj[u].emplace_back(v);
         }
-        for(int i=0; i<E; i++){
-            int u, v; cin >> u >> v;
-            u--; v--;
-            adj[u].push_back(v);
-            radj[v].push_back(u);
-        }
-        vector<int> tsrt = topological_sort();
-        for(auto i:tsrt){
-            if(!rvis[i]){
-                rdfs(i);
-                cout << endl;
+        Mx ans = findSCC(n,adj);
+        cout<<"strongly connected components :\n";
+        for(auto x:ans){
+            for(auto y : x){
+                cout<<y<<" ";
             }
+            cout<<"\n";
         }
     }
 }
